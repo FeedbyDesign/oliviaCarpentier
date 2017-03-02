e56da7909cc0e70ed7d71e4b079f690c54e28b6a
Marc Coet <marcoet@gmail.com>
2017-03-02 20:00:26 +0100

    testing responsiveness

--- src/Footer.js
+++ src/Footer.js
@@ -5,7 +5,7 @@ export default () => {
     <footer>
       <div>
         <p><i className="fa fa-copyright" aria-hidden="true"></i> Olivia Carpentier</p>
-        <p>Conception web originale <a className="feed" href="http://<<<.feedbydesign.com" target="_blank">Feed by Design</a></p>
+        <p>Conception web originale <a className="feed" href="http://www.feedbydesign.com" target="_blank">Feed by Design</a></p>
       </div>
     </footer>
   )
